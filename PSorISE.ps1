2b20353b09268a5b34395f484ff9d8dd3b1dece6
Tenaka <86342641+Tenaka@users.noreply.github.com>
2021-06-23 17:09:41 +0100

    Add files via upload

---
+++ PSorISE.ps1
@@ -0,0 +1,17 @@
+﻿    #Working area for scripts   
+    #Detect PS or ISE
+    $working = "C:\Working"
+
+    if($psise -ne $null)
+    {
+        $ISEPath = $psise.CurrentFile.FullPath
+        $ISEWork = $ISEPath.TrimEnd("NameofScript.ps1")
+        New-Item -Path $working -ItemType Directory -Force
+
+    }
+    else
+    {
+        $PSWork = split-path -parent $MyInvocation.MyCommand.Path
+        New-Item -Path $working-ItemType Directory -Force
+          
+    }
